d6d9ec86b67ac99f08919fd23b59193906a5283b
Thomalita Burbank <thomalita37@gmail.com>
2022-12-09 13:09:06 -0700

    updated anime routes

--- routes/AnimeRouter.js
+++ routes/AnimeRouter.js
@@ -1,10 +1,15 @@
 const Router = require('express').Router()
 const controller = require('../controllers/AnimeController')
+const middleware = require('../middleware')
+
 
 Router.get('/', controller.GetAllAnime)
 Router.post('/', controller.CreateAnime)
-Router.put('/anime_id', controller.UpdateAnime)
-Router.delete('/anime_id', controller.DeleteAnime)
+Router.put('/:anime_id', 
+    // middleware.stripToken,
+    // middleware.verifyToken, 
+    controller.UpdateAnime)
+Router.delete('/:anime_id', controller.DeleteAnime)
 Router.get('/:anime_id', controller.GetAnimeById)
 
 
