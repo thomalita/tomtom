8aee80f77dd6bfaf35978b1ac62c0f974362673a
Thomalita Burbank <thomalita37@gmail.com>
2022-12-09 04:27:31 -0700

    added auth controller

---
+++ controllers/AuthController.js
@@ -0,0 +1,51 @@
+const { User } = require('../models')
+const middleware = require('../middleware')
+
+const Login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { email: req.body.email },
+      raw: true
+    })
+    if (
+      user &&
+      middleware.comparePassword(user.passwordDigest, req.body.password)
+    ) {
+      let payload = {
+        id: user.id,
+        email: user.email
+      }
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    throw error
+  }
+}
+
+const Register = async (req, res) => {
+  try {
+    const { profilePic, email, password, username } = req.body
+    let passwordDigest = await middleware.hashPassword(password)
+    const user = await User.create({
+      profilePic,
+      email,
+      passwordDigest,
+      username
+    })
+    res.send(user)
+  } catch (error) {
+    throw error
+  }
+}
+const CheckSession = async (req, res) => {
+  const { payload } = res.locals
+  res.send(payload)
+}
+
+module.exports = {
+  Login,
+  Register,
+  CheckSession
+}
